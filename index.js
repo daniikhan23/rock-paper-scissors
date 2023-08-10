8cccfb82673a10344d59e6f103df6553141e7a02
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 13:37:20 +0500

    playRound function added to game function to keep track of the user and computer's score, as well as displaying the winner of each round

--- index.js
+++ index.js
@@ -11,22 +11,34 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
-  let playerChoice = playerSelection.toUpperCase();
-  let computerChoice = computerSelection.toUpperCase();
+function game() {
 
-  while (playerChoice === "ROCK") {
-    if (computerChoice === "ROCK") {
-      return "Tie! You must try again";
-    } else if (computerChoice === "PAPER") {
-      return "You Lost! Paper beats Rock!";
-    } else {
-      return "Congratulations! Rock destroys Paper!";
+  let userScore = 0;
+  let computerScore = 0;
+
+  function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toUpperCase();
+    let computerChoice = computerSelection.toUpperCase();
+  
+    while (playerChoice === "ROCK") {
+      if (computerChoice === "ROCK") {
+        return "Tie! You must try again" + " You: " + userScore + " Computer: " + computerScore;
+      } else if (computerChoice === "PAPER") {
+        computerScore += 1;
+        return "You Lost! Paper beats Rock!" + " You: " + userScore + " Computer: " + computerScore;
+      } else {
+        userScore += 1;
+        return "Congratulations! Rock destroys Scissors!" + " You: " + userScore + " Computer: " + computerScore;
+      }
     }
   }
+  
+  const playerSelection = prompt("Please enter your choice of Rock, Paper or Scissors!");
+  console.log(playerSelection)
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  
+  console.log(playRound(playerSelection, computerSelection));
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
