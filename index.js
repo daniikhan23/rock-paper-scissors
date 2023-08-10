b032e071461677b65b7294d23b2f1ea95676ef7a
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 14:29:18 +0500

    Show final score and declare winner at the end of the game

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function getComputerChoice() {
   }
 }
 
+// Function to actually play the game and appropriately determin winner
 function game() {
 
   let userScore = 0;
@@ -28,63 +29,71 @@ function game() {
     let computerChoice = computerSelection.toUpperCase();
     console.log("Computer's Choice: " + computerChoice);
 
-
+    // Check to see correct input
     if (playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS"){
     
       //Player Choice : Rock
-    if (playerChoice === "ROCK") {
-      if (computerChoice === "ROCK") {
-        return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else if (computerChoice === "PAPER") {
-        computerScore += 1;
-        return "You Lost! Paper beats Rock!" + ", You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else {
-        userScore += 1;
-        return "Congratulations! Rock destroys Scissors!" + ", You: " + userScore + ", Computer: " + computerScore;
+      if (playerChoice === "ROCK") {
+        if (computerChoice === "ROCK") {
+          return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else if (computerChoice === "PAPER") {
+          computerScore += 1;
+          return "You Lost! Paper beats Rock!" + ", You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else {
+          userScore += 1;
+          return "Congratulations! Rock destroys Scissors!" + ", You: " + userScore + ", Computer: " + computerScore;
+        }
+      }
+
+      //Player Choice : Paper
+      if (playerChoice === "PAPER") {
+        if (computerChoice === "ROCK") {
+          userScore += 1;
+          return "Congratulations! Paper smothered Rock to death" + ", You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else if (computerChoice === "PAPER") {
+          return "Tie! You must try again" + " You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else {
+          computerScore += 1;
+          return "You Lost! Scissors cut up Paper" + ", You: " + userScore + ", Computer: " + computerScore;
+        }
+      }
+
+      //Player Choice : Scissors
+      if (playerChoice === "SCISSORS") {
+        if (computerChoice === "ROCK") {
+          computerScore += 1;
+          return "You Lost! Rock destroys Scissors" + ", You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else if (computerChoice === "PAPER") {
+          userScore += 1;
+          return "Congratulations! Scissors cut up Paper" + " You: " + userScore + ", Computer: " + computerScore;
+        } 
+        else {
+          return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
+        }
+      }
+      } else {
+        return "Please enter a valid choice";
       }
     }
 
-    //Player Choice : Paper
-    if (playerChoice === "PAPER") {
-      if (computerChoice === "ROCK") {
-        userScore += 1;
-        return "Congratulations! Paper smothered Rock to death" + ", You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else if (computerChoice === "PAPER") {
-        return "Tie! You must try again" + " You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else {
-        computerScore += 1;
-        return "You Lost! Scissors cut up Paper" + ", You: " + userScore + ", Computer: " + computerScore;
-      }
-    }
+    console.log(playRound(askUser(), getComputerChoice()));
+    console.log(playRound(askUser(), getComputerChoice()));
+    console.log(playRound(askUser(), getComputerChoice()));
+    console.log(playRound(askUser(), getComputerChoice()));
+    console.log(playRound(askUser(), getComputerChoice()));
 
-    //Player Choice : Scissors
-    if (playerChoice === "SCISSORS") {
-      if (computerChoice === "ROCK") {
-        computerScore += 1;
-        return "You Lost! Rock destroys Scissors" + ", You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else if (computerChoice === "PAPER") {
-        userScore += 1;
-        return "Congratulations! Scissors cut up Paper" + " You: " + userScore + ", Computer: " + computerScore;
-      } 
-      else {
-        return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
-      }
-    }
+    if (userScore > computerScore) {
+      alert("You won! " + "Your Score: " + userScore + ", Computer's Score: " + computerScore);
+    } else if (userScore < computerScore) {
+      alert("You Lost! " + "Your Score: " + userScore + ", Computer's Score: " + computerScore);
     } else {
-      return "Please enter a valid choice";
+      alert("Seriously? A tie? How Disappointing " + "Your Score: " + userScore + ", Computer's Score: " + computerScore);
     }
-  }
-  
-  console.log(playRound(askUser(), getComputerChoice()));
-  console.log(playRound(askUser(), getComputerChoice()));
-  console.log(playRound(askUser(), getComputerChoice()));
-  console.log(playRound(askUser(), getComputerChoice()));
-  console.log(playRound(askUser(), getComputerChoice()));
 }
 
 game();
