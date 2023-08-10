99c3a7c79f5e76adba50f4c7e0cc883c08ecb1ea
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 13:49:51 +0500

    playRound fully functional, also checks to see if user has made a valid choice

--- index.js
+++ index.js
@@ -19,14 +19,20 @@ function game() {
   function playRound(playerSelection, computerSelection) {
     let playerChoice = playerSelection.toUpperCase();
     let computerChoice = computerSelection.toUpperCase();
+
+    if (playerChoice !== "ROCK" || playerChoice !== "PAPER" || playerChoice !== "SCISSORS") {
+      return "Please make a valid choice";
+    }
   
     while (playerChoice === "ROCK") {
       if (computerChoice === "ROCK") {
         return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
-      } else if (computerChoice === "PAPER") {
+      } 
+      else if (computerChoice === "PAPER") {
         computerScore += 1;
         return "You Lost! Paper beats Rock!" + ", You: " + userScore + ", Computer: " + computerScore;
-      } else {
+      } 
+      else {
         userScore += 1;
         return "Congratulations! Rock destroys Scissors!" + ", You: " + userScore + ", Computer: " + computerScore;
       }
@@ -35,11 +41,26 @@ function game() {
       if (computerChoice === "ROCK") {
         userScore += 1;
         return "Congratulations! Paper smothered Rock to death" + ", You: " + userScore + ", Computer: " + computerScore;
-      } else if (computerChoice === "PAPER") {
+      } 
+      else if (computerChoice === "PAPER") {
         return "Tie! You must try again" + " You: " + userScore + ", Computer: " + computerScore;
-      } else {
+      } 
+      else {
         computerScore += 1;
         return "You Lost! Scissors cut up Paper" + ", You: " + userScore + ", Computer: " + computerScore;
+      }
+    }
+    while (playerChoice === "SCISSORS") {
+      if (computerChoice === "ROCK") {
+        computerScore += 1;
+        return "You Lost! Rock destroys Scissors" + ", You: " + userScore + ", Computer: " + computerScore;
+      } 
+      else if (computerChoice === "PAPER") {
+        userScore += 1;
+        return "Congratulations! Scissors cut up Paper" + " You: " + userScore + ", Computer: " + computerScore;
+      } 
+      else {
+        return "Tie! You must try again" + ", You: " + userScore + ", Computer: " + computerScore;
       }
     }
   }
