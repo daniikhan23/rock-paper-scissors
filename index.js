749b6656c7a18c58dff514ad17143baa9e6694df
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 13:26:27 +0500

    Added playRound function that determines the winner between the player's choice and the computer's choice

--- index.js
+++ index.js
@@ -11,5 +11,22 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  let playerChoice = playerSelection.toUpperCase();
+  let computerChoice = computerSelection.toUpperCase();
 
+  while (playerChoice === "ROCK") {
+    if (computerChoice === "ROCK") {
+      return "Tie! You must try again";
+    } else if (computerChoice === "PAPER") {
+      return "You Lost! Paper beats Rock!";
+    } else {
+      return "Congratulations! Rock destroys Paper!";
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
