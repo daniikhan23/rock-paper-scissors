4b2fe7caac2f5fa8dbbab34e4575331598d8e77f
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 13:15:18 +0500

    computerChoice function now returns the correct string options of Rock, Paper or Scissor

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("Hello World!");
+// function to get computer's choice of options
+
+function getComputerChoice() {
+  let num = Math.floor(Math.random() * 3) + 1;
+  if (num == 1) { 
+    return "Rock";
+  } else if (num == 2) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+console.log(getComputerChoice());
+
