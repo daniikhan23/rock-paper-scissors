2ed57714c956a0abb59f61b62560145f87c00bfb
Daniyal Ashraf Khan <dani.ashraf@outlook.com>
2023-08-10 15:48:31 +0500

    Refactored Code: Replaced getComputerChoice function's if loops with switch cases Added a getScore function that adds to the end of every end of round or game message the score

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // function to get user's choice of option
-function askUser() {
+function getUserChoice() {
   const playerSelection = prompt("Please enter your choice of Rock, Paper or Scissors!");
   return playerSelection;
 }
@@ -8,13 +8,21 @@ function askUser() {
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3) + 1;
-  if (num == 1) { 
-    return "Rock";
-  } else if (num == 2) {
-    return "Paper";
-  } else {
-    return "Scissors";
+  switch (num) {
+    case 1:
+      return "Rock";
+      break;
+    case 2:
+      return "Paper";
+      break;
+    default:
+      return "Scissors";
   }
+}
+
+// Calculate Score
+function getScore(userScore, computerScore) {
+  return "You: " + userScore + " - " + computerScore + " Computer";
 }
 
 // Function to actually play the game and appropriately determine winner
@@ -48,15 +56,15 @@ function game() {
       //Player Choice : Rock
       if (playerChoice === "ROCK") {
         if (computerChoice === "ROCK") {
-          return "Round " + gameRounds + ": " + " Tie! You must try again" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + " Tie! You must try again! " + getScore(userScore, computerScore);
         } 
         else if (computerChoice === "PAPER") {
           computerScore += 1;
-          return "Round " + gameRounds + ": " + "You Lost! Paper beats Rock!" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "You Lost! Paper beats Rock! " + getScore(userScore, computerScore);
         } 
         else {
           userScore += 1;
-          return "Round " + gameRounds + ": " + "Congratulations! Rock destroys Scissors!" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "Congratulations! Rock destroys Scissors! " + getScore(userScore, computerScore);
         }
       }
 
@@ -64,14 +72,14 @@ function game() {
       else if (playerChoice === "PAPER") {
         if (computerChoice === "ROCK") {
           userScore += 1;
-          return "Round " + gameRounds + ": " + "Congratulations! Paper smothered Rock to death" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "Congratulations! Paper smothered Rock to death! " + getScore(userScore, computerScore);
         } 
         else if (computerChoice === "PAPER") {
-          return "Round " + gameRounds + ": " + "Tie! You must try again" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "Tie! You must try again! " + getScore(userScore, computerScore);
         } 
         else {
           computerScore += 1;
-          return "Round " + gameRounds + ": " + "You Lost! Scissors cut up Paper" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "You Lost! Scissors cut up Paper! " + getScore(userScore, computerScore);
         }
       }
 
@@ -79,14 +87,14 @@ function game() {
       else if (playerChoice === "SCISSORS") {
         if (computerChoice === "ROCK") {
           computerScore += 1;
-          return "Round " + gameRounds + ": " + "You Lost! Rock destroys Scissors" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "You Lost! Rock destroys Scissors! " + getScore(userScore, computerScore);
         } 
         else if (computerChoice === "PAPER") {
           userScore += 1;
-          return "Round " + gameRounds + ": " + "Congratulations! Scissors cut up Paper" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "Congratulations! Scissors cut up Paper! " + getScore(userScore, computerScore);
         } 
         else {
-          return "Round " + gameRounds + ": " + "Tie! You must try again" + ", You: " + userScore + " - " + computerScore + " Computer";
+          return "Round " + gameRounds + ": " + "Tie! You must try again! " + getScore(userScore, computerScore);
         }
       }
     }
@@ -96,16 +104,16 @@ function game() {
   }
 
     while (gameRounds <= 5) {
-      alert(playRound(askUser(), getComputerChoice()));
+      alert(playRound(getUserChoice(), getComputerChoice()));
       gameRounds += 1;
     }
 
     if (userScore > computerScore) {
-      alert("You won the game! " + "You: " + userScore + " - " + computerScore + " Computer");
+      alert("You won the game! " + getScore(userScore, computerScore));
     } else if (userScore < computerScore) {
-      alert("You Lost the game! " + "You: " + userScore + " - " + computerScore + " Computer");
+      alert("You Lost the game! " + getScore(userScore, computerScore));
     } else {
-      alert("Seriously? A tie? How Disappointing " + "You: " + userScore + " - " + computerScore + " Computer");
+      alert("Seriously? A tie? How Disappointing " + getScore(userScore, computerScore));
     }
 }
 
